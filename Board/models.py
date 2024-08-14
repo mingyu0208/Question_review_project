323f8055162935f6b9dcd9c79904ab7151a842d7
mingyu0208 <mingyu9980920@gmail.com>
2024-08-14 16:18:22 +0900

    질문 수정,삭제 commit

--- Board/models.py
+++ Board/models.py
@@ -5,6 +5,7 @@ class Question(db.Model):
     subject = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+    modify_date = db.Column(db.DateTime(), nullable=True)
 
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -12,4 +13,6 @@ class Answer(db.Model):
     question = db.relationship('Question', backref=db.backref('answer_set',))
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+    modify_date = db.Column(db.DateTime(), nullable=True)
+
 
--- Board/views/question_views.py
+++ Board/views/question_views.py
@@ -4,19 +4,22 @@ from werkzeug.utils import redirect
 from .. import db
 
 from Board.models import Question
-from ..forms import QuestionForm
+from ..forms import QuestionForm, AnswerForm
 
 bp = Blueprint('question', __name__, url_prefix='/question')
 
 @bp.route('/list/')
 def _list():
+    page = request.args.get('page', type=int, default=1)
     question_list = Question.query.order_by(Question.create_date.desc())
+    question_list = question_list.paginate(page=page, per_page=10)
     return render_template('question/question_list.html', question_list=question_list)
 
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html', question = question)
+    return render_template('question/question_detail.html', question = question, form=form )
 
 @bp.route('/create', methods=('GET', 'POST'))
 def create():
@@ -27,3 +30,26 @@ def create():
         db.session.commit()
         return redirect(url_for('main.index'))
     return render_template('question/question_form.html', form = form)
+
+
+@bp.route('/modify/<int:question_id>', methods=('GET', 'POST'))
+def modify(question_id):
+    question = Question.query.get_or_404(question_id)
+    if request.method == 'POST':
+        form = QuestionForm()
+        if form.validate_on_submit():
+            form.populate_obj(question)
+            question.modify_date = datetime.now()
+            db.session.commit()
+            return redirect(url_for('question.detail', question_id=question_id))
+    else:
+        form = QuestionForm(obj=question)
+    return render_template('question/question_form.html', form=form)
+
+# 삭제 코드
+@bp.route('/delete/<int:question_id>')
+def delete(question_id):
+    question = Question.query.get_or_404(question_id)
+    db.session.delete(question)
+    db.session.commit()
+    return redirect(url_for('question._list'))
